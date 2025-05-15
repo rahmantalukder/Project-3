6c732b2be56b3f469bc46e36e429c07f9824ff5c
rahmantalukder <comt210621@dti.ac>
2025-05-16 00:44:25 +0600

    java file chanage

--- main.java
+++ main.java
@@ -2,5 +2,10 @@ public class main {
     int a = 5;
     int b = 10;
     double total = a + b;
-
+    double average = total / 2;
+    public static void main(String[] args) {
+        main obj = new main();
+        System.out.println("Total: " + obj.total);
+        System.out.println("Average: " + obj.average);
+    }
 }
